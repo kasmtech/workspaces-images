f373c0afb1e07ee0033cf8b1523abb12527a6ca1
Dmitry Maksyoma <ledestin@gmail.com>
2021-07-13 21:56:06 +1200

    Refactor

--- src/ubuntu/install/gtk/install_restricted_file_chooser.sh
+++ src/ubuntu/install/gtk/install_restricted_file_chooser.sh
@@ -5,5 +5,5 @@ set -e
 libgtk_deb=libgtk.deb
 
 wget https://kasmweb-build-artifacts.s3.amazonaws.com/kasm-gtk-3-restricted-file-chooser/de486e8c3c5f3d3c0f898fb9d6e05755897b1970/output/libgtk-3-0_3.22.30-1ubuntu4_amd64.deb -O $libgtk_deb
-apt-get install -y --allow-downgrades ./$libgtk_deb
+apt-get install -y --allow-downgrades ./"$libgtk_deb"
 rm "$libgtk_deb"
