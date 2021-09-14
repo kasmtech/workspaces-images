f1b208d8390b457f993523ea04df65c94b515197
Ian Tangney <ian.tangney@kasmweb.com>
2021-09-14 19:27:30 +0000

    KASM-1362 Update Tor-Browser to support persistent profiles

--- src/ubuntu/install/torbrowser/install_torbrowser.sh
+++ src/ubuntu/install/torbrowser/install_torbrowser.sh
@@ -3,26 +3,33 @@ set -xe
 echo "Install TorBrowser"
 
 apt-get install -y xz-utils curl
+TOR_HOME=$HOME/tor-browser/
+mkdir -p $TOR_HOME
 TOR_URL=$(curl -q https://www.torproject.org/download/ | grep downloadLink | grep linux64 | sed 's/.*href="//g'  | cut -d '"' -f1 | head -1)
 wget --quiet https://www.torproject.org/${TOR_URL} -O /tmp/torbrowser.tar.xz
-tar -xJf /tmp/torbrowser.tar.xz -C /tmp/
-
-# Patch up the Tor Launcher file so we can move the icon to the deskop but not the folder
-cp /tmp/tor-browser_en-US/start-tor-browser.desktop /tmp/tor-browser_en-US/start-tor-browser.desktop.bak
-sed -i 's/^Name=.*/Name=Tor Browser/g' /tmp/tor-browser_en-US/start-tor-browser.desktop
-sed -i 's/Icon=.*/Icon=\/tmp\/tor-browser_en-US\/Browser\/browser\/chrome\/icons\/default\/default128.png/g' /tmp/tor-browser_en-US/start-tor-browser.desktop
-sed -i 's/Exec=.*/Exec=sh -c \x27"\/tmp\/tor-browser_en-US\/Browser\/start-tor-browser" --detach || ([ !  -x "\/tmp\/tor-browser_en-US\/Browser\/start-tor-browser" ] \&\& "$(dirname "$*")"\/Browser\/start-tor-browser --detach)\x27 dummy %k/g'  /tmp/tor-browser_en-US/start-tor-browser.desktop
+tar -xJf /tmp/torbrowser.tar.xz -C $TOR_HOME
+rm /tmp/torbrowser.tar.xz
 
 
-cat >>/tmp/tor-browser_en-US/Browser/TorBrowser/Data/Browser/profile.default/prefs.js <<EOL
+cp $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop.bak
+cp $TOR_HOME/tor-browser_en-US/Browser/browser/chrome/icons/default/default128.png /usr/share/icons/tor.png
+chown 1000:0 /usr/share/icons/tor.png
+sed -i 's/^Name=.*/Name=Tor Browser/g' $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop
+sed -i 's/Icon=.*/Icon=\/usr\/share\/icons\/tor.png/g' $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop
+sed -i 's/Exec=.*/Exec=sh -c \x27"$HOME\/tor-browser\/tor-browser_en-US\/Browser\/start-tor-browser" --detach || ([ !  -x "$HOME\/tor-browser\/tor-browser_en-US\/Browser\/start-tor-browser" ] \&\& "$(dirname "$*")"\/Browser\/start-tor-browser --detach)\x27 dummy %k/g'  $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop
+
+
+cat >> $TOR_HOME/tor-browser_en-US/Browser/TorBrowser/Data/Browser/profile.default/prefs.js <<EOL
 user_pref("extensions.torlauncher.prompt_at_startup", false);
 user_pref("browser.download.lastDir", "/home/kasm-user/Downloads");
 EOL
 
-chown -R 1000:0 /tmp/tor-browser_en-US/
+# Maintain backwards compatability with old image definitions that expect tor to be launched from /tmp
+mkdir -p /tmp/tor-browser_en-US/Browser/
+ln -s $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop /tmp/tor-browser_en-US/Browser/start-tor-browser.desktop 
+
+chown -R 1000:0 $TOR_HOME/
 
 
-cp /tmp/tor-browser_en-US/start-tor-browser.desktop $HOME/Desktop/
+cp $TOR_HOME/tor-browser_en-US/start-tor-browser.desktop $HOME/Desktop/
 chown 1000:0  $HOME/Desktop/start-tor-browser.desktop
-
-rm /tmp/torbrowser.tar.xz
