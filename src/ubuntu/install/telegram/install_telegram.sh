40cbc7eef37e6964f6cbe30a939188c7b2c6033f
Justin Travis <justin@kasmweb.com>
2021-10-27 20:28:22 -0400

    KASM-2010 Add additional arm supported images.

--- src/ubuntu/install/telegram/install_telegram.sh
+++ src/ubuntu/install/telegram/install_telegram.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -ex
+
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
+
+if [ "${ARCH}" == "arm64" ] ; then
+    echo "Telegram for arm64 currently not supported, skipping install"
+    exit 0
+fi
+
+
 wget -q https://telegram.org/dl/desktop/linux -O /tmp/telegram.tgz
 tar -xvf /tmp/telegram.tgz -C /opt/
 rm -rf /tmp/telegram.tgz
