6cf21230e39950bf0e875015e62ca5d9f31ac005
Richard Koliser <richard.koliser@kasmweb.com>
2022-08-15 16:39:20 +0000

    KASM-3073 Add a proper version sort

--- src/ubuntu/install/vs_code/install_vs_code.sh
+++ src/ubuntu/install/vs_code/install_vs_code.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 wget -q https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O vs_code_amd64.deb
-dpkg -i vs_code_amd64.deb
+apt-get update
+apt-get install -y ./vs_code_amd64.deb
 sed -i 's#/usr/share/code/code#/usr/share/code/code --no-sandbox##' /usr/share/applications/code.desktop
 cp /usr/share/applications/code.desktop $HOME/Desktop
 chmod +x $HOME/Desktop/code.desktop
@@ -14,3 +15,10 @@ apt-get install -y python3-setuptools \
                    python3-venv \
                    python3-virtualenv \
                    maximus
+
+# Cleanup
+apt-get autoclean
+rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
