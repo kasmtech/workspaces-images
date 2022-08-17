f0f573273dc98208e818247d36099e0e8960f71b
Justin Travis <justin@kasmweb.com>
2022-08-17 03:27:56 +0000

    Resolve KASM-1991 "Feature/ gamepad support"

--- src/ubuntu/install/zsnes/install_zsnes.sh
+++ src/ubuntu/install/zsnes/install_zsnes.sh
@@ -4,5 +4,10 @@ dpkg --add-architecture i386
 apt-get update
 apt-get install -y zsnes
 
+mkdir $HOME/.zsnes
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_PATH="$(realpath $SCRIPT_PATH)"
+cp ${SCRIPT_PATH}/zinput.cfg $HOME/.zsnes/zinput.cfg
 
+chown -R 1000:1000 $HOME/.zsnes
