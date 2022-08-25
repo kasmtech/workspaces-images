6df373a261260e4560f92214e65cf9da1e5f9d56
ryan.kuba <ryan.kuba@kasmweb.com>
2022-08-25 10:51:53 -0400

    KASM-3107 add tor browser changes to 1.10.0 release branch

--- src/ubuntu/install/torbrowser/install_torbrowser.sh
+++ src/ubuntu/install/torbrowser/install_torbrowser.sh
@@ -23,6 +23,13 @@ cat >> $TOR_HOME/tor-browser_en-US/Browser/TorBrowser/Data/Browser/profile.defau
 user_pref("extensions.torlauncher.prompt_at_startup", false);
 user_pref("extensions.torlauncher.quickstart", true);
 user_pref("browser.download.lastDir", "/home/kasm-user/Downloads");
+user_pref("torbrowser.settings.bridges.builtin_type", "");
+user_pref("torbrowser.settings.bridges.enabled", false);
+user_pref("torbrowser.settings.bridges.source", -1);
+user_pref("torbrowser.settings.enabled", true);
+user_pref("torbrowser.settings.firewall.enabled", false);
+user_pref("torbrowser.settings.proxy.enabled", false);
+user_pref("torbrowser.settings.quickstart.enabled", true);
 EOL
 
 # Maintain backwards compatability with old image definitions that expect tor to be launched from /tmp
