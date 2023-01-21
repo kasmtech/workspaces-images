3e8eb23952c4ecbb2f913f798608ee7b56ddda13
ryan.kuba <ryan.kuba@kasmweb.com>
2023-01-21 13:34:01 -0800

    KASM-3934 bad URL for arm64

--- src/ubuntu/install/torbrowser/install_torbrowser.sh
+++ src/ubuntu/install/torbrowser/install_torbrowser.sh
@@ -7,7 +7,7 @@ TOR_HOME=$HOME/tor-browser/
 mkdir -p $TOR_HOME
 if [ "$(arch)" == "aarch64" ]; then
   SF_VERSION=$(curl -sI https://sourceforge.net/projects/tor-browser-ports/files/latest/download | awk -F'(ports/|/tor)' '/location/ {print $3}')
-  FULL_TOR_URL="https://downloads.sourceforge.net/project/tor-browser-ports/${SF_VERSION}/tor-browser-linux-arm64-${SF_VERSION}.tar.xz"
+  FULL_TOR_URL="https://downloads.sourceforge.net/project/tor-browser-ports/${SF_VERSION}/tor-browser-linux-arm64-${SF_VERSION}_ALL.tar.xz"
 else
   TOR_URL=$(curl -q https://www.torproject.org/download/ | grep downloadLink | grep linux64 | sed 's/.*href="//g'  | cut -d '"' -f1 | head -1)
   FULL_TOR_URL="https://www.torproject.org/${TOR_URL}"
