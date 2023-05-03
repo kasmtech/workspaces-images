1fe00e7bf7ce95407b66943970c26f15b8cc0bca
Ryan Kuba <ryan.kuba@kasmweb.com>
2023-05-03 20:06:00 +0000

    Resolve KASM-4367 "Feature/ discord updates"

--- src/ubuntu/install/discord/install_discord.sh
+++ src/ubuntu/install/discord/install_discord.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 set -ex
-apt-get update
 
+# Install Discord from deb
+apt-get update
 curl -L -o discord.deb  "https://discord.com/api/download?platform=linux&format=deb"
 apt-get install -y ./discord.deb
 rm discord.deb
+
+# Default config values
+mkdir -p $HOME/.config/discord/
+echo '{"SKIP_HOST_UPDATE": true}' > $HOME/.config/discord/settings.json
+
+# Desktop file setup
 sed -i "s@Exec=/usr/share/discord/Discord@Exec=/usr/share/discord/Discord --no-sandbox@g"  /usr/share/applications/discord.desktop
 cp /usr/share/applications/discord.desktop $HOME/Desktop/
 chmod +x $HOME/Desktop/discord.desktop
+
+# Cleanup
+if [ -z ${SKIP_CLEAN+x} ]; then
+    apt-get autoclean
+    rm -rf \
+        /var/lib/apt/lists/* \
+        /var/tmp/* \
+        /tmp/*
+fi
