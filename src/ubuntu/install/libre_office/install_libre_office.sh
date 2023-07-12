5dcf5653440dbce8d370699dc5b988bd18a82dd0
ryan.kuba <ryan.kuba@kasmweb.com>
2023-07-11 20:18:58 -0400

    KASM-4563 add modifications to desktop launcher for libreoffice in Ubuntu

--- src/ubuntu/install/libre_office/install_libre_office.sh
+++ src/ubuntu/install/libre_office/install_libre_office.sh
@@ -13,6 +13,14 @@ rm -rf \
   /var/lib/apt/lists/* \
   /var/tmp/*
 
+sed -i "s@Exec=libreoffice@Exec=env LD_LIBRARY_PATH=:/usr/lib/libreoffice/program:/usr/lib/$(arch)-linux-gnu/ libreoffice@g" /usr/share/applications/libreoffice-*.desktop
 cp /usr/share/applications/libreoffice-startcenter.desktop $HOME/Desktop/
 chown 1000:1000 $HOME/Desktop/libreoffice-startcenter.desktop
 chmod +x $HOME/Desktop/libreoffice-startcenter.desktop
+
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/*
+fi
