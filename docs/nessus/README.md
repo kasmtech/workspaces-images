eb17cce4953168e5cf7bccb75789651fd1e81b82
Justin Travis <justin@kasmweb.com>
2024-01-01 11:19:40 -0500

    KASM-5299 Cleanup

--- docs/nessus/README.md
+++ docs/nessus/README.md
@@ -4,7 +4,7 @@ This Image contains a browser-accessible version of [Tennable Nessus](https://ww
 
 ![Screenshot][Image_Screenshot]
 
-[Image_Screenshot]: https://5856039.fs1.hubspotusercontent-na1.net/hubfs/5856039/dockerhub/deluge.png "Image Screenshot"
+[Image_Screenshot]: https://5856039.fs1.hubspotusercontent-na1.net/hubfs/5856039/dockerhub/image-screenshots/nessus.png "Image Screenshot"
 
 # Environment Variables
 
--- src/ubuntu/install/nessus/custom_startup.sh
+++ src/ubuntu/install/nessus/custom_startup.sh
@@ -5,46 +5,7 @@ PGREP="nessusd"
 DEFAULT_ARGS=""
 ARGS=${APP_ARGS:-$DEFAULT_ARGS}
 
-options=$(getopt -o gau: -l go,assign,url: -n "$0" -- "$@") || exit
-eval set -- "$options"
-
-while [[ $1 != -- ]]; do
-    case $1 in
-        -g|--go) GO='true'; shift 1;;
-        -a|--assign) ASSIGN='true'; shift 1;;
-        -u|--url) OPT_URL=$2; shift 2;;
-        *) echo "bad option: $1" >&2; exit 1;;
-    esac
-done
-shift
-
-# Process non-option arguments.
-for arg; do
-    echo "arg! $arg"
-done
-
 FORCE=$2
-
-kasm_exec() {
-    if [ -n "$OPT_URL" ] ; then
-        URL=$OPT_URL
-    elif [ -n "$1" ] ; then
-        URL=$1
-    fi 
-    
-    # Since we are execing into a container that already has the browser running from startup, 
-    #  when we don't have a URL to open we want to do nothing. Otherwise a second browser instance would open. 
-    if [ -n "$URL" ] ; then
-        /usr/bin/filter_ready
-        /usr/bin/desktop_ready
-        bash ${MAXIMIZE_SCRIPT} &
-        $START_COMMAND $ARGS $OPT_URL &
-        sleep 3
-        chromium-browser https://localhost:8834 --start-maximized
-    else
-        echo "No URL specified for exec command. Doing nothing."
-    fi
-}
 
 kasm_startup() {
     if [ -n "$KASM_URL" ] ; then
@@ -78,8 +39,4 @@ kasm_startup() {
 
 } 
 
-if [ -n "$GO" ] || [ -n "$ASSIGN" ] ; then
-    kasm_exec
-else
-    kasm_startup
-fi
+kasm_startup
