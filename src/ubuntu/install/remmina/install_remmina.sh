24a9727ffe5f500c651dbc3769bff06ddefd0732
Ryan Kuba <ryan.kuba@kasmweb.com>
2024-01-11 11:39:34 +0000

    KASM-5403 KASM-5399 fix chromium bug that will not launch if singleton locks...

--- src/ubuntu/install/remmina/install_remmina.sh
+++ src/ubuntu/install/remmina/install_remmina.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ "${DISTRO}" == @(centos|oracle7|oracle8|oracle9|rockylinux9|rockylinux8|almalinux9|almalinux8|fedora37|fedora38) ]]; then
+if [[ "${DISTRO}" == @(centos|oracle7|oracle8|oracle9|rockylinux9|rockylinux8|almalinux9|almalinux8|fedora37|fedora38|fedora39) ]]; then
   if [[ "${DISTRO}" == @(oracle8|rockylinux8|almalinux8) ]]; then
     dnf install -y remmina remmina-plugins-rdp remmina-plugins-secret remmina-plugins-spice xdotool
     if [ -z ${SKIP_CLEAN+x} ]; then
       dnf clean all
     fi
-  elif [[ "${DISTRO}" == @(rockylinux9|oracle9|almalinux9|fedora37|fedora38) ]]; then
+  elif [[ "${DISTRO}" == @(rockylinux9|oracle9|almalinux9|fedora37|fedora38|fedora39) ]]; then
     dnf install -y remmina remmina-plugins-rdp remmina-plugins-secret xdotool
     if [ -z ${SKIP_CLEAN+x} ]; then
       dnf clean all
@@ -25,7 +25,7 @@ elif [ "${DISTRO}" == "opensuse" ]; then
   fi
 elif grep -q "ID=debian" /etc/os-release; then
   apt-get update
-  apt-get install -y remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice xdotool
+  apt-get install -y remmina remmina-plugin-rdp remmina-plugin-secret xdotool
   if [ -z ${SKIP_CLEAN+x} ]; then
   apt-get autoclean
   rm -rf \
