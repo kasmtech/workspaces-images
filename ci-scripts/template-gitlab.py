c07438795bd63f6411779156605b774e8f3c3a53
Ryan Kuba <ryan.kuba@kasmweb.com>
2024-02-06 20:34:25 +0000

    Resolve KASM-5423 "Feature/ named schedules"

--- ci-scripts/template-gitlab.py
+++ ci-scripts/template-gitlab.py
@@ -6,10 +6,15 @@ import os
 
 # Determine if this is a feature branch
 fileLimits = True
+scheduled = 'NO'
+scheduleName = 'NO'
 if os.getenv('SANITIZED_BRANCH').startswith('release') or os.getenv('SANITIZED_BRANCH') == 'develop':
   fileLimits = False
 if os.getenv('CI_PIPELINE_SOURCE') == 'schedule':
   fileLimits = False
+  scheduled = 'YES'
+if 'SCHEDULE_NAME' in os.environ:
+  scheduleName = os.getenv('SCHEDULE_NAME')
 if os.getenv('USE_PRIVATE_IMAGES') == 1:
   fileLimits = False
 
@@ -21,6 +26,8 @@ with open("template-vars.yaml", 'r') as stream:
   templateVars['USE_PRIVATE_IMAGES'] = os.getenv('USE_PRIVATE_IMAGES')
   templateVars['BASE_TAG'] = os.getenv('BASE_TAG')
   templateVars['FILE_LIMITS'] = fileLimits
+  templateVars['SCHEDULED'] = scheduled
+  templateVars['SCHEDULE_NAME'] = scheduleName
 
 # Read template file
 with open("gitlab-ci.template", 'r') as stream:
