0a1eae11e928b59f173370f12503fe7981b5feaa
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-07-24 13:21:36 +0800

    bake in ict133

--- src/valence/install/labs/ict133.sh
+++ src/valence/install/labs/ict133.sh
@@ -1,17 +1,22 @@
-Step 1:
+#!/bin/bash
+
+# Step 1: Clone repositories
 cd /
 git clone https://$PAT@github.com/suss-vli/ilabguide.git
 git clone https://$PAT@github.com/suss-vli/suss.git
 git clone https://$PAT@github.com/suss-vli/learntools.git
 
-Step 2:
-Files and folders to copy from ilabguide repository to the respective folders. You need to create these directories:
+unset PAT
 
-ilabguide/ict133/ict133_questions/lab1-6.ipynb > $HOME/Desktop/labguide
-ilabguide/ict133/config_ict133.yml > /opt/suss/customscripts/.config.yml
-ilabguide/ict133/.encrypted_ict133_solution/* > /opt/suss/customscripts/ict133/.encrypted_ict133_solution
+# Step 2: Copy files and create directories
+mkdir -p $HOME/Desktop/ilabguide
+mkdir -p /opt/suss/customscripts/ict133/.encrypted_ict133_solution
 
-Step 3:
+cp ilabguide/ict133/ict133_questions/lab1-6.ipynb $HOME/Desktop/ilabguide/
+cp ilabguide/ict133/config_ict133.yml /opt/suss/customscripts/.config.yml
+cp -r ilabguide/ict133/.encrypted_ict133_solution/* /opt/suss/customscripts/ict133/.encrypted_ict133_solution/
+
+# Step 3: Install packages
 cd /
 pip3 install ./suss
 pip3 install ./learntools
