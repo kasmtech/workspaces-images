4cd94a669d63069c9c0833cd274468f16cf28f26
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-07-25 05:23:06 +0800

    Update wallpaper.sh

--- src/valence/install/wallpaper/wallpaper.sh
+++ src/valence/install/wallpaper/wallpaper.sh
@@ -9,15 +9,15 @@ WALLPAPER_FILE="official_suss_wallpaper.png"
 
 # Source and destination paths
 SOURCE_PATH="$SCRIPT_DIR/$WALLPAPER_FILE"
-DEST_PATH="$HOME/background/$WALLPAPER_FILE"
+# DEST_PATH="$HOME/background/$WALLPAPER_FILE"
 
 # Copy the wallpaper file
-echo "Copying wallpaper to $DEST_PATH..."
-cp "$SOURCE_PATH" "$DEST_PATH"
+# echo "Copying wallpaper to $DEST_PATH..."
+# cp "$SOURCE_PATH" "$DEST_PATH"
 
 # Set the wallpaper
 echo "Setting wallpaper..."
-gsettings set org.gnome.desktop.background picture-uri "file://$DEST_PATH"
+gsettings set org.gnome.desktop.background picture-uri "file://$SOURCE_PATH"
 gsettings set org.gnome.desktop.background picture-options 'scaled'
 
 echo "Wallpaper has been set successfully!"
