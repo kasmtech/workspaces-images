c74f3b47457c513404546addaec94ed10d9878de
suss-su <janetanjy@suss.edu.sg>
2024-07-25 17:25:21 +0800

    move .config.yml, add gcm and set default app for .ipynb to vscode

--- src/valence/install/labs/ict133.sh
+++ src/valence/install/labs/ict133.sh
@@ -13,7 +13,7 @@ mkdir -p $HOME/Desktop/ilabguide
 mkdir -p /opt/suss/customscripts/ict133/.encrypted_ict133_solution
 
 cp ilabguide/ict133/ict133_questions/* $HOME/Desktop/ilabguide/
-cp ilabguide/ict133/config_ict133.yml /opt/suss/customscripts/.config.yml
+cp ilabguide/ict133/config_ict133.yml $HOME/Desktop/ilabguide/.config.yml
 cp -r ilabguide/ict133/.encrypted_ict133_solution/* /opt/suss/customscripts/ict133/.encrypted_ict133_solution/
 
 # Step 3: Install packages
@@ -21,3 +21,11 @@ cd /
 pip3 install ./suss
 pip3 install ./learntools
 pip3 install -r ./ilabguide/ict133/requirements.txt
+
+wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.1.0/gcm-linux_amd64.2.1.0.deb && \
+dpkg -i gcm-linux_amd64.2.1.0.deb && \
+rm -rfv gcm-linux_amd64.2.1.0.deb
+
+mkdir -p /home/kasm-user/.local/share/applications && \
+echo '[Default Applications]' > /home/kasm-user/.local/share/applications/mimeapps.list && \
+echo 'application/x-ipynb+json=code.desktop' >> /home/kasm-user/.local/share/applications/mimeapps.list
