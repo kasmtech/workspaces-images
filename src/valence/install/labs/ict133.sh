65a8826bee87eb4713d1b0714d658614a65edba7
suss-su <janetanjy@suss.edu.sg>
2024-07-26 15:29:01 +0800

    fix issue on .ipynb opening with vscode

--- src/valence/install/labs/ict133.sh
+++ src/valence/install/labs/ict133.sh
@@ -10,11 +10,15 @@ unset PAT
 
 # Step 2: Copy files and create directories
 mkdir -p $HOME/Desktop/ilabguide
-mkdir -p /opt/suss/customscripts/ict133/.encrypted_ict133_solution
-
 cp ilabguide/ict133/ict133_questions/* $HOME/Desktop/ilabguide/
 cp ilabguide/ict133/config_ict133.yml $HOME/Desktop/ilabguide/.config.yml
+
+mkdir -p /opt/suss/customscripts/ict133/.encrypted_ict133_solution
 cp -r ilabguide/ict133/.encrypted_ict133_solution/* /opt/suss/customscripts/ict133/.encrypted_ict133_solution/
+
+mkdir -p $HOME/.local/share/applications && \
+echo '[Default Applications]' > $HOME/.local/share/applications/mimeapps.list && \
+echo 'application/x-ipynb+json=code.desktop' >> $HOME/.local/share/applications/mimeapps.list
 
 # Step 3: Install packages
 cd /
@@ -25,7 +29,3 @@ pip3 install -r ./ilabguide/ict133/requirements.txt
 wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.1.0/gcm-linux_amd64.2.1.0.deb && \
 dpkg -i gcm-linux_amd64.2.1.0.deb && \
 rm -rfv gcm-linux_amd64.2.1.0.deb
-
-mkdir -p /home/kasm-user/.local/share/applications && \
-echo '[Default Applications]' > /home/kasm-user/.local/share/applications/mimeapps.list && \
-echo 'application/x-ipynb+json=code.desktop' >> /home/kasm-user/.local/share/applications/mimeapps.list
