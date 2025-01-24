b2ab19aa5704d8986a95c65d5ad26d91068e9efd
stanfrbd <stanfrbd@github.com>
2025-01-24 11:34:00 +0100

    My try to add Cyberbro KASM Workspace image
    Signed-off-by: Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>

---
+++ docs/cyberbro/README.md
@@ -0,0 +1,11 @@
+# About This Image
+
+This Image contains a browser-accessible version of [Cyberbro](https://github.com/stanfrbd/cyberbro).
+
+![Screenshot][Image_Screenshot]
+
+[Image_Screenshot]: https://5856039.fs1.hubspotusercontent-na1.net/hubfs/5856039/dockerhub/image-screenshots/<TO_BE_DEFINED> "Image Screenshot"
+
+# Environment Variables
+
+* `FIREFOX_APP_ARGS` - Additional arguments to pass to firefox when launched.
---
+++ src/ubuntu/install/cyberbro/install_cyberbro.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -xe
+
+CYBERBRO_VERSION=$(curl -sX GET "https://api.github.com/repos/stanfrbd/cyberbro/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
+
+# Install Spiderfoot
+echo "Install Cyberbro"
+apt-get update
+apt-get install -y python3-pip git supervisor
+CYBERBRO_HOME=$HOME/spiderfoot
+mkdir -p $CYBERBRO_HOME
+cd $CYBERBRO_HOME
+wget https://github.com/stanfrbd/cyberbro/archive/${CYBERBRO_VERSION}.tar.gz
+tar zxvf ${CYBERBRO_VERSION}.tar.gz
+rm ${CYBERBRO_VERSION}.tar.gz
+cd cyberbro-*
+pip3 install -r requirements.txt
+cp prod/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+cat <<EOF > secrets.json
+{
+  "proxy_url": "",
+  "gui_enabled_engines": ["reverse_dns", "rdap", "ipquery", "spur", "phishtank", "threatfox", "urlscan", "google", "github", "abusix"]
+}
+EOF
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
+
