ed8eccddf6c9ff2eeba02b83bf35a1c069171feb
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-04-29 06:43:46 -0400

    silence firefox security nag - develop

--- src/ubuntu/install/firefox/install_firefox.sh
+++ src/ubuntu/install/firefox/install_firefox.sh
@@ -176,6 +176,10 @@ else
   firefox -headless -CreateProfile "kasm $HOME/.mozilla/firefox/kasm"
 fi
 
+# Silence Firefox security nag "Some of Firefox's features may offer less protection on your current operating system"
+echo 'user_pref("security.sandbox.warn_unprivileged_namespaces", false);' > $HOME/.mozilla/firefox/kasm/user.js
+chown 1000:1000 $HOME/.mozilla/firefox/kasm/user.js
+
 if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|rhel9|almalinux9|almalinux8|opensuse|fedora39|fedora40) ]]; then
   set_desktop_icon
 fi
