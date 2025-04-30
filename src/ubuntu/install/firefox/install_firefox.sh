d0271583c9d3b565adad28488c06d9516f78a187
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-04-30 05:16:36 -0400

    set default firefox profile in debian and parrot on arm

--- src/ubuntu/install/firefox/install_firefox.sh
+++ src/ubuntu/install/firefox/install_firefox.sh
@@ -190,6 +190,14 @@ cat >>$HOME/.mozilla/firefox/profiles.ini <<EOL
 Default=kasm
 Locked=1
 EOL
+elif grep -q "ID=debian" /etc/os-release || grep -q "ID=parrot" /etc/os-release; then
+  if [ "${ARCH}" != "amd64" ]; then
+    cat >>$HOME/.mozilla/firefox/profiles.ini <<EOL
+[Install3B6073811A6ABF12]
+Default=kasm
+Locked=1
+EOL
+fi
 elif [[ "${DISTRO}" != @(centos|oracle8|rockylinux9|rockylinux8|oracle9|almalinux9|almalinux8|opensuse|fedora37|fedora38|fedora39) ]]; then
 cat >>$HOME/.mozilla/firefox/profiles.ini <<EOL
 [Install4F96D1932A9F858E]
