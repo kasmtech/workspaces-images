a9223cb232313bfe0e356e7c006ab748ede45b47
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-06-12 06:09:09 -0400

    remove kasm_exec

--- src/ubuntu/install/cyberbro/custom_startup.sh
+++ src/ubuntu/install/cyberbro/custom_startup.sh
@@ -18,19 +18,6 @@ fi
 # Make GUI_ENABLED_ENGINES an environment variable
 export GUI_ENABLED_ENGINES
 
-options=$(getopt -o gau: -l go,assign,url: -n "$0" -- "$@") || exit
-eval set -- "$options"
-
-while [[ $1 != -- ]]; do
-    case $1 in
-        -g|--go) GO='true'; shift 1;;
-        -a|--assign) ASSIGN='true'; shift 1;;
-        -u|--url) OPT_URL=$2; shift 2;;
-        *) echo "bad option: $1" >&2; exit 1;;
-    esac
-done
-shift
-
 # Process non-option arguments.
 for arg; do
     echo "arg! $arg"
@@ -42,25 +29,6 @@ FORCE=$2
 if [ -f /opt/VirtualGL/bin/vglrun ] && [ ! -z "${KASM_EGL_CARD}" ] && [ ! -z "${KASM_RENDERD}" ] && [ -O "${KASM_RENDERD}" ] && [ -O "${KASM_EGL_CARD}" ] ; then
     START_COMMAND="/opt/VirtualGL/bin/vglrun -d ${KASM_EGL_CARD} $START_COMMAND"
 fi
-
-kasm_exec() {
-    if [ -n "$OPT_URL" ] ; then
-        URL=$OPT_URL
-    elif [ -n "$1" ] ; then
-        URL=$1
-    fi 
-    
-    # Since we are execing into a container that already has the browser running from startup, 
-    #  when we don't have a URL to open we want to do nothing. Otherwise a second browser instance would open. 
-    if [ -n "$URL" ] ; then
-        /usr/bin/filter_ready
-        /usr/bin/desktop_ready
-        bash ${MAXIMIZE_SCRIPT} &
-        $START_COMMAND $ARGS $OPT_URL
-    else
-        echo "No URL specified for exec command. Doing nothing."
-    fi
-}
 
 
 kasm_startup() {
@@ -92,8 +60,4 @@ kasm_startup() {
     fi
 }
 
-if [ -n "$GO" ] || [ -n "$ASSIGN" ] ; then
-    kasm_exec
-else
-    kasm_startup
-fi
+kasm_startup
