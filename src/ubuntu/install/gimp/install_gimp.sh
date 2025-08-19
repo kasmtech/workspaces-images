a936d0c11db53676bc3e0e6180cc44a47d88bd9a
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-08-19 15:34:35 +0000

    get gimp version automatically

--- src/ubuntu/install/gimp/install_gimp.sh
+++ src/ubuntu/install/gimp/install_gimp.sh
@@ -1,36 +1,17 @@
-# #!/usr/bin/env bash
-# set -ex
-
-# # Install GIMP
-# apt-get update
-# apt-get install -y gimp
-# cp /usr/share/applications/gimp.desktop $HOME/Desktop/
-# chmod +x $HOME/Desktop/gimp.desktop
-
-# # Cleanup for app layer
-# chown -R 1000:0 $HOME
-# find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
-# if [ -z ${SKIP_CLEAN+x} ]; then
-#   apt-get autoclean
-#   rm -rf \
-#     /var/lib/apt/lists/* \
-#     /var/tmp/* \
-#     /tmp/*
-# fi
-
-
-
 #!/usr/bin/env bash
 set -ex
+
 ARCH=$(uname -m | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
 mkdir -p /opt/gimp-3
 cd /opt/gimp-3
 
+# Get latest stable GIMP version
+GIMP_VERSION=$(curl -s https://www.gimp.org/downloads/ | grep -Po '(?is)current stable release of gimp is.*?\K[0-9]+\.[0-9]+\.[0-9]+')
 
 if [ "${ARCH}" == "amd64" ]; then
-  wget -q https://download.gimp.org/gimp/v3.0/linux/GIMP-3.0.4-x86_64.AppImage -O gimp.AppImage
+  wget -q https://download.gimp.org/gimp/v3.0/linux/GIMP-${GIMP_VERSION}-x86_64.AppImage -O gimp.AppImage
 else
-  wget -q https://download.gimp.org/gimp/v3.0/linux/GIMP-3.0.4-aarch64.AppImage -O gimp.AppImage
+  wget -q https://download.gimp.org/gimp/v3.0/linux/GIMP-${GIMP_VERSION}-aarch64.AppImage -O gimp.AppImage
 fi
 
 chmod +x gimp.AppImage
@@ -53,3 +34,13 @@ cp /opt/gimp-3/squashfs-root/*gimp*.desktop /usr/share/applications/gimp.desktop
 chmod +x $HOME/Desktop/gimp.desktop
 chmod +x /usr/share/applications/gimp.desktop
 
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
