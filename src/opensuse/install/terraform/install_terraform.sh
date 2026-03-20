51b05c3a666fb46782477b9a22308ac28dd6e0fa
Huan Truong <huan.truong@kasmweb.com>
2026-03-19 19:32:54 -0500

    QA-308 fix install

--- src/opensuse/install/terraform/install_terraform.sh
+++ src/opensuse/install/terraform/install_terraform.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ $(arch) == "aarch64" ]] && $(grep -q "15.6" /etc/os-release); then
-  # Have to use this specific repo instead of the generic $releasever due to how the 15.6 repo deals with arm instances
-  zypper addrepo -f "https://download.opensuse.org/repositories/systemsmanagement:terraform/openSUSE_Tumbleweed/" terraform_repo
-else
-  zypper addrepo -f "https://download.opensuse.org/repositories/systemsmanagement:terraform/\$releasever/" terraform_repo
-fi
-
 zypper install -yn \
   terraform \
   terraform-provider-aws \
